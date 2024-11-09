fb9c05735b59107c5fabca6f13cf33b5d2e925d6
naveen-m-sudo <naveen_m_sudo@outlook.com>
2024-11-08 22:52:29 -0800

    update

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,11 @@
-[org 0x7c00]
-start:
+; Naveen M 08-11-2024
 
-	call print_message
-	jmp end
+[org 0x7c00]
+
+start:
+	call _clear_screen		; clears the screen
+	call print_message		; print text
+	jmp end				; jump to end of the program
 
 ;==============================
 ; Print text message
@@ -20,6 +23,10 @@ print_message:
 	.end:
 		ret 
 
+;==============================
+; Utility functions
+;==============================
+
 _print_char:
 	mov ah, 0x0e
 	int 0x10
@@ -30,11 +37,28 @@ _read_char:
 	int 0x16
 	ret
 
+_clear_screen:
+	mov ax, 0x0003
+	int 0x10
+	ret
+
+;==============================
+; End of the program
+;==============================
 end:
 	jmp $
 
-msg db "Hello, World!", 0
+;==============================
+; Messages and buffers
+;==============================
 
+msg db "helOS!", 0
+
+command_buffer times 12 db 0
+command_buffer_len db 0
+;=================================================================
+; Fill remaining empty memory with 0 and put boot signature
+;=================================================================
 times 510-($-$$) db 0
 dw 0xaa55
 
